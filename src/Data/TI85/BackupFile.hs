aa3a381182fd3633394a5a631d9d39b86b40f887
Nigel Stepp <stepp@atistar.net>
2021-12-26 18:54:13 -0800

    Add backup files description

---
+++ src/Data/TI85/BackupFile.hs
@@ -0,0 +1,52 @@
+
+-- | This module defines the structure of a TI-85 backup file.
+-- A backup file contains system memory (data section 1), user
+-- memory (section 2), and a variable table that maps where
+-- user variables are located in data section 2.
+module Data.TI85.BackupFile where
+
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Data.Word (Word8,Word16)
+
+import Data.TI85.VarFile (TIHeader, VarType)
+
+-- | An entry in the variable table.
+-- Note: the variable table is stored in
+-- reverse byte order (including 2-byte
+-- words!).
+data VarTableEntry = VarTableEntry {
+    entryId :: Word8,
+    entryAddr :: Word16,
+    entryNameLen :: Word8,
+    entryName :: ByteString
+    } deriving Show
+
+-- | A list of variable table entries
+type VarTable = [VarTableEntry]
+
+-- | Backup-specific header. This header comes after
+-- the more general `TIHeader`.
+data TIBackupHeader = TIBackupHeader {
+    hdrDataLenOffset :: Word16, -- Always 9
+    hdrData1Len :: Word16,
+    hdrTypeID :: Word8, -- Always 0x1D
+    hdrData2Len :: Word16,
+    hdrData3Len :: Word16,
+    hdrData2Addr :: Word16
+    } deriving Show
+
+-- | The top-level structure of a backup
+-- file.
+data TIBackupFile = TIBackupFile {
+    tiHeader :: TIHeader,
+    backupHeader :: TIBackupHeader,
+    data1Len :: Word16,
+    data1 :: ByteString,
+    data2Len :: Word16,
+    data2 :: ByteString,
+    varTableLen :: Word16,
+    varTable :: VarTable,
+    backupChecksum :: Word16
+    } deriving Show
+
