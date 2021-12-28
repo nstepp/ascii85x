5492c9f866ae1ced9ed7cae239150f58ef834b1b
Nigel Stepp <stepp@atistar.net>
2021-12-28 01:56:06 -0800

    Add docs

--- src/Data/TI85/File.hs
+++ src/Data/TI85/File.hs
@@ -1,3 +1,6 @@
+
+-- | A collection of types and utilities for dealing with
+-- TI Link backup files.
 module Data.TI85.File (
     module Data.TI85.File.Variable,
     module Data.TI85.File.Backup,
@@ -7,4 +10,9 @@ module Data.TI85.File (
 import Data.TI85.File.Variable
 import Data.TI85.File.Backup
 
+-- | There are two possible file formats
+-- * Variable files contain one or more variables,
+--   encoded in a variable-specific way.
+-- * Backup files contain memory dumps, which also
+--   include variable data in a raw form.
 data TIFile = BackupFile TIBackupFile | VariableFile TIVarFile
