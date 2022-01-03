e30eaf5f91c2e7e7a8818b9ea93352f5e00dcfcd
Nigel Stepp <stepp@atistar.net>
2022-01-03 00:28:06 -0800

    Prettier output for ZRCL data

--- src/Data/TI85/Var.hs
+++ src/Data/TI85/Var.hs
@@ -79,6 +79,22 @@ showProgram (PlainText progText) = progText
 showProgram (Tokenized tokens) =
     foldMap (\(Token _ t) -> t) tokens
 
+showWinSettings :: SavedWinSettings -> Text
+showWinSettings settings =
+    "\nzThetaMin: " <> showNumber (zThetaMin settings)
+    <> "\nzThetaMax: " <> showNumber (zThetaMax settings)
+    <> "\nzThetaStep: " <> showNumber (zThetaStep settings)
+    <> "\nztPlot: " <> showNumber (ztPlot settings)
+    <> "\nztMin: " <> showNumber (ztMin settings)
+    <> "\nztMax: " <> showNumber (ztMax settings)
+    <> "\nztStep: " <> showNumber (ztStep settings)
+    <> "\nzxMin: " <> showNumber (zxMin settings)
+    <> "\nzxMax: " <> showNumber (zxMax settings)
+    <> "\nzxScl: " <> showNumber (zxScl settings)
+    <> "\nzyMin: " <> showNumber (zyMin settings)
+    <> "\nzyMax: " <> showNumber (zyMax settings)
+    <> "\nzyScl: " <> showNumber (zyScl settings)
+
 -- | Convert a Variable to Text
 showVariable :: Variable -> Text
 showVariable (TIScalar tn) = showNumber tn
@@ -96,7 +112,7 @@ showVariable (TIEquation txt) = txt
 showVariable (TIString txt) = txt
 showVariable (TIProgram pro) = showProgram pro
 showVariable (TIPicture pic) = showAsciiArt pic
-showVariable (TIZRCL settings) = showText settings
+showVariable (TIZRCL settings) = showWinSettings settings
 
 -- * IO
 
