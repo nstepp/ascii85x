3ee7a617be26c33d8a93bcd4067170780caa4d13
Nigel Stepp <stepp@atistar.net>
2022-01-02 23:06:28 -0800

    Add docs

--- src/Data/TI85/Var/Pic.hs
+++ src/Data/TI85/Var/Pic.hs
@@ -1,8 +1,11 @@
+-- | Picture variable types and utilities.
 module Data.TI85.Var.Pic (
+    -- * TI Bitmap
     TIBitmap,
     emptyBitmap,
     fromBytes,
     toBytes,
+    -- * Display utilities
     showAsciiArt,
     writePicPng
     ) where
@@ -20,23 +23,30 @@ import Numeric (showHex)
 
 import Data.TI85.Encoding (tiDecode)
 
+-- | A TI Picture variable is encoded as a packed bitmap,
+-- and is always 128x63 binary pixels.
 newtype TIBitmap = TIBitmap ByteString deriving Show
 
 _bmCols = 128
 _bmRows = 63
 _bmSizeBytes = 1008
 
+-- | A blank (all zero) bitmap
 emptyBitmap :: TIBitmap
 emptyBitmap = TIBitmap (BS.replicate _bmSizeBytes 0x0)
 
+-- | Create a bitmap from a packed bytestring. If the data
+-- is the wrong size, returns `Nothing`.
 fromBytes :: ByteString -> Maybe TIBitmap
 fromBytes bytes = if BS.length bytes == 1008
     then Just (TIBitmap bytes)
     else Nothing
 
+-- | Extract the raw bytestring from a bitmap.
 toBytes :: TIBitmap -> ByteString
 toBytes (TIBitmap bytes) = bytes
 
+-- Expands an 8-bit word into an 8-element ByteString
 explodeWord :: Word8 -> ByteString
 explodeWord w =
     let (bits,_) = BS.unfoldrN 8 (\x -> Just (x .&. 1, shiftR x 1)) w
@@ -47,14 +57,23 @@ screenColorMap :: Word8 -> Pixel8
 screenColorMap 0x0 = 0xc6
 screenColorMap _ = 0x39
 
+-- Convert a bytestring, where each bit represents a pixel into
+-- a vector of pixels suitable for use in a `DynamicImage`.
+-- Bits are mapped to pixels using a vaguely calculator-like
+-- color map (light gray background, dark gray foreground).
 bytesToPixels :: ByteString -> Vector Pixel8
 bytesToPixels =  fromList . map screenColorMap . BS.unpack . BS.concatMap explodeWord
 
+-- Create an image out of a packed bytestring.
+-- See `bytesToPixels`
 bytesToImage :: ByteString -> Int -> Int -> DynamicImage
 bytesToImage bytes width height =
     let pixels = bytesToPixels bytes
     in ImageY8 (Image width height pixels)
 
+-- | Create a text-based representation of a picture.
+-- Background pixels are rendered as spaces, while
+-- foreground are rendered as full block glyphs.
 showAsciiArt :: TIBitmap -> Text
 showAsciiArt (TIBitmap bytes) =
     let bits = BS.concatMap explodeWord bytes
@@ -72,6 +91,9 @@ showAsciiArt (TIBitmap bytes) =
     chunk n "" chunks = chunks
     chunk n xs chunks = take n xs : chunk n (drop n xs) chunks
 
+-- | Save a bitmap as a PNG image.
+-- Bits are mapped to pixels using a vaguely calculator-like
+-- color map (light gray background, dark gray foreground).
 writePicPng :: FilePath -> TIBitmap -> IO ()
 writePicPng path (TIBitmap bitmap) =
     let img = bytesToImage bitmap _bmCols _bmRows
